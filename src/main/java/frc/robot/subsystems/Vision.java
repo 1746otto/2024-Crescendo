2fa85c2bdb39f1d0ddf4a1e8536b410e9ecd11e9
programming1 <programming@team1746.com>
2024-01-20 15:01:53 -0500

    nothing

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,12 +1,33 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.math.geometry.Pose3d;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 
 public class Vision {
     Thread visionThread;
     PhotonCamera camera1;
     // PhotonCamera camera2;
     PhotonPipelineResult lastResult;
+    Pose3d camera1Pose;
+
+    public Vision() {
+        camera1 = new PhotonCamera("frontCamera");
+        lastResult = new PhotonPipelineResult();
+
+        visionThread.setName("Vision Thread");
+        visionThread = new Thread(() -> {
+            while (true) {
+                getResult();
+
+            }
+        });
+    }
+
+    private void getResult() {
+        lastResult = camera1.getLatestResult();
+    }
 }
