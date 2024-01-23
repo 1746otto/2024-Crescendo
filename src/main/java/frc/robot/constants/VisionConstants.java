3b04ca5bc93a4b227b41b4545083c7fb23ec6d95
programming1 <programming@team1746.com>
2024-01-22 19:42:13 -0500

    Added like 10 lines

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 public class VisionConstants {
     public static final double kAngleMargin = 3.0;
     public static final double kDistanceCutoff = 15.0;
+    public static final double kAmbiguityCutoff = 0.2;
     public static final Transform3d camera1Transform = new Transform3d(new Pose3d(), new Pose3d(0.5, 0, 0.5, new Rotation3d()));
     
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,7 +4,12 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.CommandSwerveDrivetrain;
+import frc.robot.constants.VisionConstants;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 
@@ -14,17 +19,37 @@ public class Vision {
     // PhotonCamera camera2;
     PhotonPipelineResult lastResult;
     Pose3d camera1Pose;
+    AprilTagFieldLayout field;
+    Pose3d tempPose;
+    CommandSwerveDrivetrain swerve;
 
-    public Vision() {
+    public Vision(CommandSwerveDrivetrain swerveDrive) {
         camera1 = new PhotonCamera("frontCamera");
         lastResult = new PhotonPipelineResult();
+        swerve = swerveDrive;
+        try {
+            field = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+        }
+        catch (Exception e) {
+            SmartDashboard.putString("Vision Error Message", e.getMessage());
+        }
 
         visionThread.setName("Vision Thread");
         visionThread = new Thread(() -> {
             while (true) {
                 getResult();
                 for (PhotonTrackedTarget target : lastResult.targets) {
-                    
+                    if (target.getFiducialId() > 0 || target.getFiducialId() <= 16)
+                        continue;
+                    if (target.getPoseAmbiguity() > VisionConstants.kAmbiguityCutoff) {
+                        tempPose = field.getTagPose(target.getFiducialId()).get()
+                            .transformBy(target.getBestCameraToTarget().inverse())
+                            .transformBy(VisionConstants.camera1Transform.inverse());
+                        if (Math.abs(tempPose.getRotation().getZ() - swerve.getRotation3d().getZ()) < VisionConstants.kAngleMargin) {
+                            
+                        }
+      
+                    }
                 }
             }
         });
