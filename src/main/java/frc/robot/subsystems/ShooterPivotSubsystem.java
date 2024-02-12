f5222ce189eff1480aa26b093050b46f43b74a97
Rohan Nair <rohanmanunair@gmail.com>
2024-02-12 17:45:24 -0500

    dumb commit to install vendordeps

--- src/main/java/frc/robot/subsystems/ShooterPivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterPivotSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterPivotSubsystem {
     public ShooterPivotSubsystem(){
+        String json = "https://frcsdk.reduxrobotics.com/ReduxLib_2024.json";
 
     }
 }
