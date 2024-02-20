93834cf20d66a91dff665ce212a2ad431a856a06
Carson Walker <orangecw10@gmail.com>
2024-02-20 06:46:24 -0500

    Added vision std deviations constant. Not tuned.

--- src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -8,10 +8,12 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.constants.VisionConstants;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
@@ -24,6 +26,9 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
 
     public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
         super(driveTrainConstants, OdometryUpdateFrequency, modules);
+
+        setVisionMeasurementStdDevs(VisionConstants.kVisionStdDeviations);
+
         if (Utils.isSimulation()) {
             startSimThread();
         }
--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -1,15 +1,19 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
-    public static final double kAngleMargin = Units.degreesToRadians(3.0);
-    public static final double kAngularPerpendicularityCutoff = Units.degreesToRadians(7.0); // Perpendicularity as in the direction facing and the plane of the tag. Maybe parallel rename to parallel cutoff?
-    public static final double kDistanceCutoff = Units.feetToMeters(15.0);
-    public static final double kAmbiguityCutoff = 0.2;
+    public static final double kAngleMargin = Units.degreesToRadians(3.0); // TODO: Tune the angle margin for the robot.
+    public static final double kAngularPerpendicularityCutoff = Units.degreesToRadians(7.0); // Perpendicularity as in the direction facing and the plane of the tag. Maybe parallel rename to parallel cutoff? TODO: Tune the perpendicularity cutoff for this robot.
+    public static final double kDistanceCutoff = Units.feetToMeters(15.0); // TODO: Tune the distance cutoff for this robot.
+    public static final double kAmbiguityCutoff = 0.2; // TODO: Tune the ambiguity cutoff for this robot. It is probably fine, but we need to test it.
     public static final String[] kCameraNames = {
         "frontCamera",
         "backCamera"
@@ -18,6 +22,7 @@ public class VisionConstants {
     public static final Transform3d[] kCameraTransforms = {
         new Transform3d(new Pose3d(), new Pose3d(0.5, 0, 0.5, new Rotation3d())), 
         new Transform3d(new Pose3d(), new Pose3d(-0.5, 0, 0.5, new Rotation3d()))
-    };
+    }; // TODO: Get the actual transforms for the cameras.
+    public static final Matrix<N3, N1> kVisionStdDeviations = VecBuilder.fill(0.9, 0.9, 0.9); // TODO: Tune these values for our robot.
     
 }
