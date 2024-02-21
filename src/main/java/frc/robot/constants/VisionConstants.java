88d62b24c006b8832cf7b82903fe6217f2372542
programming1 <programming@team1746.com>
2024-02-21 17:01:08 -0500

    alpha bot camera transform constants changes

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
@@ -14,12 +15,11 @@ public class VisionConstants {
     public static final double kDistanceCutoff = Units.feetToMeters(15.0); // TODO: Tune the distance cutoff for this robot.
     public static final double kAmbiguityCutoff = 0.2; // TODO: Tune the ambiguity cutoff for this robot. It is probably fine, but we need to test it.
     public static final String[] kCameraNames = {
-        "frontCamera",
-        "backCamera"
+        "frontCamera"
     };
     public static final int kCameraCount = kCameraNames.length;
     public static final Transform3d[] kCameraTransforms = {
-        new Transform3d(new Pose3d(), new Pose3d(0.5, 0, 0.5, new Rotation3d())), 
+        new Transform3d(new Translation3d(0.5, 0, Units.inchesToMeters(24.27 + 7.1875)), new Rotation3d(0, Units.degreesToRadians(12), 0)), 
         new Transform3d(new Pose3d(), new Pose3d(-0.5, 0, 0.5, new Rotation3d()))
     }; // TODO: Get the actual transforms for the cameras.
     public static final Vector<N3> kVisionStdDeviations = VecBuilder.fill(0.9, 0.9, 0.9); // TODO: Tune these values for our robot.
