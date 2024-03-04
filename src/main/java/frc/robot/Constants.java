1c7e4023e479b6f542e88b9aab4d4b7e4a95741f
programming1 <programming@team1746.com>
2024-03-04 17:21:24 -0500

    Add back in analog input channel

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -89,6 +89,7 @@ public final class Constants {
 
     public static final int kCurrentLimit = 60;
     // public static final double kFF = 0.0001654579973546788;
+    public static final int kShooterAnalogInputChannel = 0;
 
 
   }
