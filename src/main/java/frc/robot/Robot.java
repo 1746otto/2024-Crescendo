63317a46189cf9fa2ed30345d8bd55849c40c8c7
Cooper Meitz <coopermeitz@microsoft.com>
2024-03-24 10:04:40 -0400

    it nearly works

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,22 +4,76 @@
 
 package frc.robot;
 
+import java.sql.Driver;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.photonvision.estimation.TargetModel;
+import org.photonvision.simulation.VisionSystemSim;
+import org.photonvision.simulation.VisionTargetSim;
+
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.constants.FieldConstants;
+import frc.robot.constants.TunerConstants;
+import frc.robot.subsystems.VisionSim;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
+  private Optional<VisionSim> visionSimSubsystem;
+  private Optional<SwerveDrivePoseEstimator> poseEsimator;
+  private VisionSystemSim visionSim = new VisionSystemSim("photonSim2");
+  private Pose3d simPose = new Pose3d(13, 2.157, 2, new Rotation3d(0, 0, Math.PI));
+  private TargetModel model36h11 = TargetModel.kAprilTag36h11;
+  private VisionTargetSim visionTargetSim = new VisionTargetSim(new Pose3d(), model36h11);
+  private AprilTagFieldLayout simAprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
-    
+    visionSim.addAprilTags(simAprilTagFieldLayout);
+    visionSim.addVisionTargets(visionTargetSim);
+    visionSim.resetRobotPose(simPose);
+    if (Utils.isSimulation()) {
+      SwerveModule[] modules = new SwerveModule[4];
+      SwerveModulePosition[] modulePositions = new SwerveModulePosition[4];
+      Translation2d[] moduleLocations = new Translation2d[4];
+
+      int iteration = 0;
+      for (SwerveModuleConstants module : TunerConstants.createSimDrivetrain() ){
+        modules[iteration] = new SwerveModule(module, TunerConstants.SIM_DRIVETRAIN_CONSTANTS.CANbusName);
+        moduleLocations[iteration] = new Translation2d(module.LocationX, module.LocationY);
+        modulePositions[iteration] = modules[iteration].getPosition(true);
+
+        iteration++;
+      }
+      Supplier<Rotation3d> gyro = () -> simPose.getRotation();
+      SwerveDriveKinematics kinematics = new SwerveDriveKinematics(moduleLocations);
+      poseEsimator = Optional.of(new SwerveDrivePoseEstimator(kinematics,
+          simPose.toPose2d().getRotation(), modulePositions, simPose.toPose2d()));
+      visionSimSubsystem = Optional.of(new VisionSim(poseEsimator.get(), gyro));
+    }
     SmartDashboard.putData(m_robotContainer.autoChooser);
   }
 
@@ -30,18 +84,21 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
   public void disabledPeriodic() {
     if (m_robotContainer.autoChooser.getSelected() != m_robotContainer.currentAuton) {
-      m_robotContainer.autonCommand = m_robotContainer.drivetrain.getAutoPath(m_robotContainer.autoChooser.getSelected());
+      m_robotContainer.autonCommand = m_robotContainer.drivetrain
+          .getAutoPath(m_robotContainer.autoChooser.getSelected());
       m_robotContainer.currentAuton = m_robotContainer.autoChooser.getSelected();
     }
   }
 
   @Override
-  public void disabledExit() {}
+  public void disabledExit() {
+  }
 
   @Override
   public void autonomousInit() {
@@ -53,10 +110,12 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
-  public void autonomousExit() {}
+  public void autonomousExit() {
+  }
 
   @Override
   public void teleopInit() {
@@ -72,10 +131,12 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
-  public void teleopExit() {}
+  public void teleopExit() {
+  }
 
   @Override
   public void testInit() {
@@ -83,11 +144,17 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void testExit() {}
+  public void testExit() {
+  }
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    System.out.println(visionSim.getRobotPose());
+    simPose = simPose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(0, 0, Math.toRadians(1))));
+    visionSim.update(simPose);
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/VisionSim.java
@@ -0,0 +1,230 @@
+package frc.robot.subsystems;
+
+import java.util.function.Supplier;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.constants.VisionConstants;
+
+public class VisionSim {
+    Thread visionThread;
+    PhotonCamera[] cameras = new PhotonCamera[VisionConstants.kCameraCount];
+    public volatile PhotonPipelineResult[] lastResults = new PhotonPipelineResult[VisionConstants.kCameraCount];
+    public volatile Pose3d[] cameraPoses = new Pose3d[VisionConstants.kCameraCount];
+    public volatile Pose3d robotPose; // Might use this in other filter methods later
+    AprilTagFieldLayout field;
+    volatile Pose3d tempPose;
+    private SwerveDrivePoseEstimator poseEstimator;
+    private Supplier<Rotation3d> gyro;
+    volatile boolean continueLoop;
+    int speakerID = 7; 
+    String tags = new String();
+
+
+    public VisionSim(SwerveDrivePoseEstimator poseEstimator, Supplier<Rotation3d> gyro) {
+
+        for (int i = 0; i < VisionConstants.kCameraCount; i++) {
+            cameras[i] = new PhotonCamera(VisionConstants.kCameraNames[i]);
+            cameraPoses[i] = new Pose3d();
+        }
+
+        getResult();
+
+        this.gyro = gyro;
+        this.poseEstimator = poseEstimator;
+        this.poseEstimator.setVisionMeasurementStdDevs(VisionConstants.kVisionStdDeviations);
+
+        try {
+            field = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+        }
+        catch (Exception e) {
+            SmartDashboard.putString("Vision Error Message", e.getMessage());
+        }
+
+        visionThread = new Thread(() -> {
+            while (true) {
+                getResult();
+                
+                filter4();
+            }
+        });
+
+        if (DriverStation.getAlliance().isPresent())
+            if (DriverStation.getAlliance().get() == Alliance.Blue) {
+                speakerID = 7;
+            }
+            else {
+                speakerID = 4;
+            }
+
+        visionThread.setName("Vision Thread");
+
+        visionThread.start();
+    }
+
+    public void stopThread() {
+        visionThread.interrupt();
+    }
+
+    public void startThread() {
+        visionThread.start();
+    }
+
+    private void getResult() {
+        for (int i = 0; i < VisionConstants.kCameraCount; i++) {
+            lastResults[i] = cameras[i].getLatestResult();
+        }
+    }
+
+    private Pose3d bestTargetToRobotPose(PhotonTrackedTarget target, int cameraNumber) {
+        return field.getTagPose(target.getFiducialId()).get()
+            .transformBy(target.getBestCameraToTarget().inverse())
+            .transformBy(VisionConstants.kCameraTransforms[cameraNumber].inverse());
+    }
+
+    private Pose3d alternateTargetToRobotPose(PhotonTrackedTarget target, int cameraNumber) {
+        return field.getTagPose(target.getFiducialId()).get()
+            .transformBy(target.getBestCameraToTarget().inverse())
+            .transformBy(VisionConstants.kCameraTransforms[cameraNumber].inverse());
+    }
+
+
+    /**
+     * Was it really worth all that effort?
+     * Filter methods used:
+     * - ID: If the Fiducial ID isn't used on the field the target is discarded.
+     * - Distance: Distant tags are discarded due to degradation of the tag resolution.
+     * - Ambiguity: Tags with greater that 0.2 pose ambiguity are rejected.
+     *   The best pose transform is always used. No alternates are considered.
+     */
+    private void filter4() {
+        for (int i = 0; i < VisionConstants.kCameraCount; i++) {
+
+            SmartDashboard.putNumber("getTimestampSeconds", lastResults[i].getTimestampSeconds());
+            SmartDashboard.putNumber("FPGA Timestamp - latency", Timer.getFPGATimestamp() - lastResults[i].getLatencyMillis() / 1000.0);
+
+            for (PhotonTrackedTarget target : lastResults[i].targets) {
+
+                if (target.getFiducialId() > 16 || target.getFiducialId() < 1 || target.getPoseAmbiguity() > VisionConstants.kAmbiguityCutoff)
+                    continue;
+                
+                // Transforms to the pose of the camera, not the robot.
+                tempPose = bestTargetToRobotPose(target, i);
+                
+                /*
+                 * The Math.abs on the raw z position is only necessary if we don't know whether we are above or below the AprilTag.
+                 * Assuming I have written this correctly, the Z component of the best pose from the camera perspectective should be
+                 * reflected accross the plane where the Z is equal to the tag height. Then the distance from 0 is compared and
+                 * depending on which is smaller the best or alternate tag transform is chosen.
+                 */
+                if (tempPose.getTranslation().getNorm() < VisionConstants.kDistanceCutoff) {
+                
+                    SmartDashboard.putString(VisionConstants.kCameraNames[i] + " pose", tempPose.toString());
+
+                    // This must be here in order to try until the swerve drive unlocks the pose estimator.
+                    do {
+                        // Rohan wouldn't let me use for loop :(
+                        continueLoop = false;
+                        try {
+                            poseEstimator.addVisionMeasurement(tempPose.toPose2d(), lastResults[i].getTimestampSeconds());
+                        } catch (Exception e) {
+                            continueLoop = true;
+                        }
+                    } while (continueLoop == true);
+                }
+            }
+        }
+    }
+
+    private void filter5() {
+        for (int i = 0; i < VisionConstants.kCameraCount; i++) {
+
+            SmartDashboard.putNumber("getTimestampSeconds" + " " + Integer.toString(i), lastResults[i].getTimestampSeconds());
+            
+            SmartDashboard.putNumber("FPGA Timestamp - latency" + " " + Integer.toString(i), Timer.getFPGATimestamp() - lastResults[i].getTimestampSeconds());
+
+            for (PhotonTrackedTarget target : lastResults[i].targets) {
+
+                if (target.getFiducialId() > 16 || target.getFiducialId() < 1)
+                    continue;
+                
+                // Transforms to the pose of the camera, not the robot.
+                tempPose = field.getTagPose(target.getFiducialId()).get()
+                    .transformBy(target.getBestCameraToTarget().inverse());
+                
+                /*
+                 * The Math.abs on the raw z position is only necessary if we don't know whether we are above or below the AprilTag.
+                 * Assuming I have written this correctly, the Z component of the best pose from the camera perspectective should be
+                 * reflected accross the plane where the Z is equal to the tag height. Then the distance from 0 is compared and
+                 * depending on which is smaller the best or alternate tag transform is chosen.
+                 */
+                SmartDashboard.putBoolean("tag present", field.getTagPose(i).isPresent());
+                if (Math.abs(tempPose.getZ()) 
+                <= Math.abs(field.getTagPose(target.getFiducialId()).get()
+                .transformBy(target.getAlternateCameraToTarget())
+                .getZ())
+                    && target.getBestCameraToTarget().getTranslation().getNorm() < VisionConstants.kDistanceCutoff) {
+                    
+                    // Transforms from camera to robot pose.
+                    tempPose = tempPose.transformBy(VisionConstants.kCameraTransforms[i].inverse());
+
+                    SmartDashboard.putString(VisionConstants.kCameraNames[i] + " pose", tempPose.toString());
+                    
+                    if (target.getFiducialId() == speakerID) {
+                        cameraPoses[i] = tempPose;
+                    }
+                    
+                    // This must be here in order to try until the swerve drive unlocks the pose estimator.
+                    do {
+                        // Rohan wouldn't let me use for loop :(
+                        continueLoop = false;
+                        try {
+                            
+                            poseEstimator.addVisionMeasurement(tempPose.toPose2d(), lastResults[i].getTimestampSeconds());
+                        } catch (Exception e) {
+                            continueLoop = true;
+                        }
+                    } while (continueLoop == true);
+                    
+                    continue;
+                }
+                
+                tempPose = alternateTargetToRobotPose(target, i);
+
+                // I should check if the normal is the same on both flipped and unflipped tags.
+                // Could decrease the verbosity of the function quite a bit.
+                if (Math.abs(tempPose.getRotation().getZ() - gyro.get().getZ()) < VisionConstants.kAngleMargin
+                    && target.getAlternateCameraToTarget().getTranslation().getNorm() < VisionConstants.kDistanceCutoff) {
+                    
+                    SmartDashboard.putString(VisionConstants.kCameraNames[i] + " pose", tempPose.toString());
+                    
+                    if (target.getFiducialId() == speakerID) {
+                        cameraPoses[i] = tempPose;
+                    }
+                    
+                    // This must be here in order to try until the swerve drive unlocks the pose estimator.
+                    do {
+                        continueLoop = false;
+                        try {
+                            //cameraPoses[i] = tempPose;
+                            poseEstimator.addVisionMeasurement(tempPose.toPose2d(), lastResults[i].getTimestampSeconds());
+                        } catch (Exception e) {
+                            continueLoop = true; // This could all be fixed with a goto...
+                        }
+                    } while (continueLoop == true);
+                }
+            }
+        }
+    }
+}
