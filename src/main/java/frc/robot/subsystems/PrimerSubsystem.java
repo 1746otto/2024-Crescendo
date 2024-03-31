225056f5a6da690f794107faf6b86ca8b9aafa0a
Cooper Meitz <coopermeitz@gatech.edu>
2024-03-31 18:59:33 -0400

    primer subsystem changes

--- src/main/java/frc/robot/subsystems/PrimerSubsystem.java
+++ src/main/java/frc/robot/subsystems/PrimerSubsystem.java
@@ -18,18 +18,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
-
 /**
- * Class for PrimerSubsystem to move game pieces from indexer to a holding space near the shooter.
+ * Class for PrimerSubsystem to move game pieces from indexer to a holding space
+ * near the shooter.
  */
-public class PrimerSubsystem extends SubsystemBase{
+public class PrimerSubsystem extends SubsystemBase {
 
   private TalonFX primerRoller;
   private AnalogInput speakerBeamBreak;
   public boolean primerStow = false;
   public double tempPosition;
-
- 
 
   /**
    * Creates a new PrimerSubsystem with initialized motor controller.
@@ -38,55 +36,57 @@ public class PrimerSubsystem extends SubsystemBase{
     primerRoller = new TalonFX(PrimerConstants.kPrimerRollerMotorID);
     TalonFXConfiguration configs = new TalonFXConfiguration();
     configs.CurrentLimits = new CurrentLimitsConfigs()
-      .withStatorCurrentLimit(PrimerConstants.kStatorLimit)
-      .withSupplyCurrentLimit(PrimerConstants.kSupplyLimit);
+        .withStatorCurrentLimit(PrimerConstants.kStatorLimit)
+        .withSupplyCurrentLimit(PrimerConstants.kSupplyLimit);
     configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     configs.Slot0 = new Slot0Configs()
-      .withKP(PrimerConstants.kVelocityP)
-      .withKI(PrimerConstants.kVelocityI)
-      .withKD(PrimerConstants.kVelocityD)
-      .withKS(PrimerConstants.kVelocityS)
-      .withKV(PrimerConstants.kVelocityV)
-      .withKA(PrimerConstants.kVelocityA);
+        .withKP(PrimerConstants.kVelocityP)
+        .withKI(PrimerConstants.kVelocityI)
+        .withKD(PrimerConstants.kVelocityD)
+        .withKS(PrimerConstants.kVelocityS)
+        .withKV(PrimerConstants.kVelocityV)
+        .withKA(PrimerConstants.kVelocityA);
     configs.Slot1 = new Slot1Configs()
-      .withKP(PrimerConstants.kPositionP)
-      .withKI(PrimerConstants.kPositionI)
-      .withKD(PrimerConstants.kPositionD)
-      .withKS(PrimerConstants.kPositionS)
-      .withKV(PrimerConstants.kPositionV)
-      .withKA(PrimerConstants.kPositionA);
-    
+        .withKP(PrimerConstants.kPositionP)
+        .withKI(PrimerConstants.kPositionI)
+        .withKD(PrimerConstants.kPositionD)
+        .withKS(PrimerConstants.kPositionS)
+        .withKV(PrimerConstants.kPositionV)
+        .withKA(PrimerConstants.kPositionA);
+
     primerRoller.getConfigurator().apply(configs);
 
-    
     primerRoller.setInverted(false);
     speakerBeamBreak = new AnalogInput(ShooterConstants.kShooterAnalogInputChannel);
     // pidController.setP(PrimerConstants.kP);
     // pidController.setI(PrimerConstants.kI);
     // pidController.setFF(PrimerConstants.kFF);
-
+    tempPosition = primerRoller.getPosition().getValueAsDouble();
   }
 
   /**
    * Sets the priming roller speed for priming the shooting mechanism.
    */
   public void primeNote(double rpm) {
-    //pidController.setReference(rpm, ControlType.kVelocity);
+    // pidController.setReference(rpm, ControlType.kVelocity);
   }
-  
+
   public void setSpeed(double speed) {
     primerRoller.set(speed);
   }
+
   /**
    * Run the motor backwards at a slow speed of kPrimerReverseSpeed.
    */
-  
+
   public void returnNote() {
     primerRoller.set(PrimerConstants.kOuttake);
   }
+
   public void note() {
     primerRoller.set(PrimerConstants.kIntake);
   }
+
   /**
    * Stops the primer motor
    */
@@ -96,49 +96,57 @@ public class PrimerSubsystem extends SubsystemBase{
 
   /**
    * Checks if the game piece is pinched into the primer at a certain point.
+   * 
    * @return true or false if game piece is held well in primer.
    */
   public boolean isObjectPinchedInPrimer() {
     return (isPrimerBeamBreakBroken());
   }
-  public boolean priming(){
+
+  public boolean priming() {
     return (primerRoller.getVelocity().getValueAsDouble() != 0);
   }
 
   /** Returns the velocity of the motor */
   // public double getVelocity() {
-  //   return pidController.getSmartMotionMaxVelocity(PrimerConstants.kPrimerSlotID);
+  // return
+  // pidController.getSmartMotionMaxVelocity(PrimerConstants.kPrimerSlotID);
   // }
 
-
- 
   public Command ampCommand() {
     return setSpeedCommand(PrimerConstants.kAmp);
   }
 
   /**
    * Runs the primer forward until the beambreak is broken.
-   * <p> Ends on: 
+   * <p>
+   * Ends on:
    * <ul>
-   *   <li> Beambreak broken
+   * <li>Beambreak broken
    * </ul>
-   * <p> End Behavior: 
+   * <p>
+   * End Behavior:
    * <ul>
-   *   <li> Whether interrupted or not, sets speed to 0 upon finishing.
+   * <li>Whether interrupted or not, sets speed to 0 upon finishing.
    * </ul>
+   * 
    * @return Command
    */
   public Command intakeCommand() {
     if (Utils.isSimulation()) {
       return new WaitCommand(2.5);
     }
-    return setSpeedCommand(PrimerConstants.kIntake).andThen(new WaitUntilCommand(this::isPrimerBeamBreakBroken)).andThen(new InstantCommand(() -> {primerStow = true; tempPosition = primerRoller.getPosition().getValueAsDouble();})).finallyDo(() -> setSpeed(0));
+    return setSpeedCommand(PrimerConstants.kIntake).andThen(new WaitUntilCommand(this::isPrimerBeamBreakBroken))
+        .andThen(new InstantCommand(() -> {
+          primerStow = true;
+          tempPosition = primerRoller.getPosition().getValueAsDouble();
+        }));
   }
 
   public Command fastIntakeCommand() {
     return setSpeedCommand(.5).andThen(new WaitUntilCommand(this::isPrimerBeamBreakBroken)).andThen(backupCommand());
   }
-  
+
   public Command setIntakeSpeed() {
     return runOnce(() -> setSpeed(PrimerConstants.kIntake));
   }
@@ -146,9 +154,11 @@ public class PrimerSubsystem extends SubsystemBase{
   public Command outtakeCommand() {
     return setSpeedCommand(PrimerConstants.kOuttake).finallyDo(() -> setSpeed(0));
   }
+
   public Command shootCommand() {
     return setSpeedCommand(PrimerConstants.kShoot).finallyDo(() -> setSpeed(0));
   }
+
   public Command stopCommand() {
     return setSpeedCommand(PrimerConstants.kStop);
   }
@@ -158,28 +168,31 @@ public class PrimerSubsystem extends SubsystemBase{
   }
 
   public Command backupCommand() {
-    return runOnce(() -> setSpeed(-.1)).andThen(new WaitUntilCommand(() -> isPrimerBeamBreakBroken()).withTimeout(0.15)).finallyDo(() -> setSpeed(0));
+    return runOnce(() -> setSpeed(-.1)).andThen(new WaitUntilCommand(() -> isPrimerBeamBreakBroken()).withTimeout(0.15))
+        .finallyDo(() -> setSpeed(0));
   }
 
   public Command setSpeedCommand(double speed) {
     return runOnce(() -> setSpeed(speed));
   }
-  public boolean isPrimerBeamBreakBroken() { //To change 
-    
+
+  public boolean isPrimerBeamBreakBroken() { // To change
+
     return ((Math.floor(speakerBeamBreak.getVoltage()) == 0));
-}
+  }
+
   @Override
   public void periodic() {
-    //System.out.println(isObjectPinchedInPrimer()); //To change
+    // System.out.println(isObjectPinchedInPrimer()); //To change
     SmartDashboard.putBoolean("Beambreak", isPrimerBeamBreakBroken());
     if (primerStow && isPrimerBeamBreakBroken()) {
-      //setSpeed(PrimerConstants.kIntake);
+      // setSpeed(PrimerConstants.kIntake);
     }
     SmartDashboard.putNumber("actual primer pos", primerRoller.getPosition().getValueAsDouble());
     SmartDashboard.putNumber("primer beambreak pos", tempPosition);
     if (primerStow) {
-       primerRoller.setControl(new PositionVoltage(tempPosition).withSlot(1));
+      primerRoller.setControl(new PositionVoltage(tempPosition).withSlot(1));
     }
   }
-  
+
 }
